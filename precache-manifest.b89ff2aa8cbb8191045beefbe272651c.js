ab80aa85870d4a225e326b6b3ed4e5d627313a0c
waiwai2525 <waiwai2525@users.noreply.github.com>
2022-02-12 12:48:09 +0000

    deploy: c2e2b6ed30a4155837150d41e1927b23d18bc8e4

---
+++ precache-manifest.b89ff2aa8cbb8191045beefbe272651c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad2f2fd0bf8637190de",
+    "url": "/bon-voyage/css/app.6736d48b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/bon-voyage/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2d889d641b6ca0923d2818923dd13cdc",
+    "url": "/bon-voyage/index.html"
+  },
+  {
+    "revision": "4677437a4a55d313ea2e",
+    "url": "/bon-voyage/js/about.f1825856.js"
+  },
+  {
+    "revision": "6ad2f2fd0bf8637190de",
+    "url": "/bon-voyage/js/app.3566bc25.js"
+  },
+  {
+    "revision": "5cc848464d3f8fb33095",
+    "url": "/bon-voyage/js/chunk-vendors.773e859e.js"
+  },
+  {
+    "revision": "7e1b2a8b2fa040d1e05fdca5f6e861e0",
+    "url": "/bon-voyage/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bon-voyage/robots.txt"
+  }
+]);
