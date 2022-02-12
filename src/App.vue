1765fdc77d14cc94bd344a62422f3811405564ad
waiwai2525 <waiwai.cps@gmail.com>
2022-02-12 22:23:27 +0900

    chore: Apply vuetify ui

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer v-model="drawer" app stateless>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Menu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-divider></v-divider>
 
-      <v-spacer></v-spacer>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Bon Voyage</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -50,7 +40,19 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      {
+        title: 'Home',
+        icon: 'mdi-home',
+        to: '/',
+      },
+      {
+        title: 'About',
+        icon: 'mdi-information',
+        to: '/about',
+      },
+    ],
   }),
 });
 </script>
